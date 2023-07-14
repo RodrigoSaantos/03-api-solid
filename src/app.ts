7ef66fc5c2bc0174d6e5c6d6ce4af8a4ad5c6735
Rodrigo Santos <rodrigo.silva@gcbinvestimentos.com>
2023-07-14 17:38:13 -0300

    Fundamentals of Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Rodrigo Santos',
+    email: 'contato.rodrigosaantos@gmail.com',
+  },
+})
+
+// ORM - Object Relational Mapper
